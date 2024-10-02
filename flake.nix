f9e87267c4505d1f2e09ff11e0fe462a2eb932e2
floppyMike <36990990+floppyMike@users.noreply.github.com>
2024-10-02 14:41:58 +0200

    Default zig

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  description = "deadcards";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
+      # Dev time (developing tools)
+      devInputs = with pkgs; [
+        zls
+      ];
+      # Build time (build tools; header libs)
+      nativeBuildInputs = with pkgs; [
+        zig
+      ];
+      # Run time (libs to link with)
+      buildInputs = with pkgs; [
+      ];
+
+    in {
+    # Utilized by `nix develop`
+    devShell.x86_64-linux = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+      name = "deadcards";
+      inherit buildInputs;
+      nativeBuildInputs = nativeBuildInputs ++ devInputs;
+    };
+
+    # Utilized by `nix build`
+    defaultPackage.x86_64-linux = pkgs.clangStdenv.mkDerivation rec {
+      pname = "deadcards";
+      version = "0.1.0";
+      src = ./.;
+
+      inherit nativeBuildInputs;
+      inherit buildInputs;
+
+      dontConfigure = true;
+      dontInstall = true;
+      doCheck = true;
+
+      buildPhase = ''
+        zig build install --cache-dir $$(pwd)/zig-cache --global-cache-dir $$(pwd)/.cache -Doptimize=ReleaseSafe --prefix $out
+      '';
+      installPhase = ''
+        zig build test --cache-dir $$(pwd)/zig-cache --global-cache-dir $$(pwd)/.cache
+      '';
+    };
+
+    # Utilized by `nix run`
+    apps.x86_64-linux = {
+      type = "app";
+      program = self.packages.x86_64-linux.deadcards;
+    };
+  };
+}
